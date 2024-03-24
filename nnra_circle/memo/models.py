64d68eadd5197fa6d1d5a16e479e25b9414aa185
Code bee <codebee345@outlook.com>
2024-03-24 22:53:54 +0100

    updated memo models

--- nnra_circle/memo/models.py
+++ nnra_circle/memo/models.py
@@ -6,7 +6,8 @@ class Memo(models.Model):
     title = models.CharField(max_length =250, null=False, blank=False)
     body = models.TextField()
     image = models.ImageField(upload_to='memoimage/%Y/%M/%d', default='def-memo-img.png')
-    
+    created = models.DateTimeField(auto_now_add=True)
+    updated= models.DateTimeField(auto_now=True)
 class MemoDocument(models.Model):
     memo = models.ForeignKey(Memo, on_delete=models.CASCADE, null=False, blank=False)
     document = models.FileField(upload_to='memodoc/%Y/%M/%d', blank=False, null=False)
