0d10c18b457545908b903360fb3d3e07f2f58ab9
Code bee <codebee345@outlook.com>
2024-03-25 12:35:57 +0100

    dep

--- nnra_circle/circle/urls.py
+++ nnra_circle/circle/urls.py
@@ -3,7 +3,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name='circle'
+app_name = 'circle'
+
 urlpatterns=[
     path('', views.home, name='home')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
