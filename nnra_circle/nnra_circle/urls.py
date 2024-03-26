b3bde8a4620f16163c8a336a9692479b07abb2bd
Code bee <codebee345@outlook.com>
2024-03-26 13:01:24 +0100

    changed urls

--- nnra_circle/nnra_circle/urls.py
+++ nnra_circle/nnra_circle/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('memo/', include("memo.urls", namespace='memo'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
