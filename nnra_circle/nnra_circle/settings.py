1c4bec642432534602d019202b297a76f6c52961
Code bee <codebee345@outlook.com>
2024-03-26 15:53:53 +0100

    lhlhl

--- nnra_circle/nnra_circle/settings.py
+++ nnra_circle/nnra_circle/settings.py
@@ -140,7 +140,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#specifies where to keep all static files
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#specifies where to keep all static files
+else:
+    STATIC_ROOT = '/home/nnraict/static/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]#specifies where to draw static fiels from
 
 
