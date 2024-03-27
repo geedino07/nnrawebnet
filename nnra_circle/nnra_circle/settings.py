a9eacac0e7d4590b7fb4e58309a776346016aee7
Code bee <codebee345@outlook.com>
2024-03-27 04:45:55 +0100

    changes

--- nnra_circle/nnra_circle/settings.py
+++ nnra_circle/nnra_circle/settings.py
@@ -138,12 +138,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/home/kyrianstatic/'
 
 if DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#specifies where to keep all static files
 else:
-    STATIC_ROOT = '/home/nnraict/static/'
+    STATIC_ROOT = '/home/kyrian/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]#specifies where to draw static fiels from
 
