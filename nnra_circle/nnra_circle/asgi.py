d4363ebee825a740605b44615a4867c93617b54b
Code bee <codebee345@outlook.com>
2024-04-01 23:17:13 +0100

    de

--- nnra_circle/nnra_circle/asgi.py
+++ nnra_circle/nnra_circle/asgi.py
@@ -8,16 +8,18 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nnra_circle.settings')
+
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 from django.conf import settings
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nnra_circle.settings')
-settings.configure()
+# settings.configure()
 
 django_asgi_app = get_asgi_application()
 
