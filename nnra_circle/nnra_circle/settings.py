1b155309a4cc02c5fcd07e587ad7b4b2bdf1392e
Code bee <codebee345@outlook.com>
2024-04-08 00:49:57 +0100

    fixed spacing error in settings

--- nnra_circle/nnra_circle/settings.py
+++ nnra_circle/nnra_circle/settings.py
@@ -202,10 +202,10 @@ if DEBUG:
     }
 else:
     CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+        "default": {
+            "BACKEND": "channels_redis.core.RedisChannelLayer",
+            "CONFIG": {
+                "hosts": [("127.0.0.1", 6379)],
+            },
         },
-    },
-}
+    }
