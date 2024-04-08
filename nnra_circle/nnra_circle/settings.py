dbf7ab9e0c5b9fc1933823e3e3a7c5a7b0eec065
Code bee <codebee345@outlook.com>
2024-04-08 15:35:12 +0100

    enabled redis channel layer

--- nnra_circle/nnra_circle/settings.py
+++ nnra_circle/nnra_circle/settings.py
@@ -210,18 +210,11 @@ else:
 #         },
 #     }
 
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels_redis.core.RedisChannelLayer",
-#         "CONFIG": {
-#             "hosts": [("127.0.0.1", 6379)],
-#         },
-#     },
-# }
-
-
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
